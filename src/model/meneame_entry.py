6b550089dce80749fd5edf89fb75be1afe0ee8b8
Luis Serrano <emilianoserranoborras33@gmail.com>
2025-02-14 17:18:43 +0100

    feat: scraper soporta paginacion, y va guardando en csvs temporales, etc

--- src/model/meneame_entry.py
+++ src/model/meneame_entry.py
@@ -1,35 +1,20 @@
+from dataclasses import dataclass, asdict
+
 # Clase para representar una "entrada" en meneame
+@dataclass
 class MeneameEntry:
-    def __init__(self,
-                 id: int,
-                 title: str,
-                 meneos: int,
-                 clicks: int,
-                 karma: int,
-                 positive_votes: int,
-                 anonymous_votes: int,
-                 negative_votes: int,
-                 category: str,
-                 comments: int,
-                 published_timestamp: int,
-                 scraped_timestamp: int):
+    id: int
+    title: str
+    meneos: int
+    clicks: int
+    karma: int
+    positive_votes: int
+    anonymous_votes: int
+    negative_votes: int
+    category: str
+    comments: int
+    published_timestamp: int
+    scraped_timestamp: int
 
-        self.id = id
-        self.title = title
-        self.meneos = meneos
-        self.clicks = clicks
-        self.karma = karma
-        self.positive_votes = positive_votes
-        self.anonymous_votes = anonymous_votes
-        self.negative_votes = negative_votes
-        self.category = category
-        self.comments = comments
-        self.published_timestamp = published_timestamp
-        self.scraped_timestamp = scraped_timestamp
-
-    def __str__(self):
-        return f"{self.id} # {self.title} # {self.published_timestamp}"
-
-    def __repr__(self):
-        return f"{self.id} # {self.title} # {self.published_timestamp}"
-
+    def to_dict(self):
+        return asdict(self)
